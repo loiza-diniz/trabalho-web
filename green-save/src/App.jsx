b33b3eb4608c7fd8c4d27db0c9c903886f0c651f
luiza <d.luiza@aluno.ifsp.edu.br>
2025-06-26 10:05:43 -0300

    commit1

---
+++ green-save/src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <h1>Oi! Eu sou o site React</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ green-save/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Esse é o rodapé da minha casinha! 🏡</p>
+    </footer>
+  );
+}
+
+export default Footer;
